0ec12c353698a5c41a690e04c46f932c492191ef
Abdullah Omar <fishilike.fish@gmail.com>
2022-03-14 21:56:08 +0600

    fix: #2 depend on fetching sample data from source url rather than local files

--- server/controllers/restaurant.controller.ts
+++ server/controllers/restaurant.controller.ts
@@ -1,20 +1,29 @@
+import 'isomorphic-fetch';
 import { Request, Response } from 'express';
 import Restaurant from '../models/restaurant.model';
 import User from '../models/user.model';
-
-import restaurantSampleData from '../sampleData/restaurant_with_menu.json';
-import userSampleData from '../sampleData/users_with_purchase_history.json';
 import { breakDownStringDate, convertTimeToMinutes } from '../utils/processSampleData';
 
 const populateDatabase = async (req: Request, res: Response) => {
 	try {
+		const getRestaurantSampleData = fetch(
+			'https://gist.githubusercontent.com/seahyc/b9ebbe264f8633a1bf167cc6a90d4b57/raw/021d2e0d2c56217bad524119d1c31419b2938505/restaurant_with_menu.json'
+		).then((res) => res.json());
+		const getUserSampleData = fetch(
+			'https://gist.githubusercontent.com/seahyc/de33162db680c3d595e955752178d57d/raw/785007bc91c543f847b87d705499e86e16961379/users_with_purchase_history.json'
+		).then((res) => res.json());
+
+		let [restaurantSampleData, userSampleData] = await Promise.all([
+			getRestaurantSampleData,
+			getUserSampleData,
+		]);
+
 		// need to update break openingHours value to {day, start, end}
 		// e.g: {day:"Mon", start:660, end:900}
-		let restaurantSampleDataCopy: any[] = [...restaurantSampleData];
 
-		restaurantSampleDataCopy = breakDownStringDate(restaurantSampleDataCopy);
+		(restaurantSampleData as any) = breakDownStringDate(restaurantSampleData as any);
 
-		await Restaurant.insertMany(restaurantSampleDataCopy, { ordered: false });
+		await Restaurant.insertMany(restaurantSampleData, { ordered: false });
 		await User.insertMany(userSampleData, { ordered: false });
 		return res.status(200).json({ msg: 'Database populated!' });
 	} catch (error) {
@@ -58,8 +67,6 @@ const searchByTime = async (req: Request, res: Response) => {
 };
 
 const searchByRestaurantOrDishName = async (req: Request, res: Response) => {
-	const page: number = (req.query as any).page ? parseInt((req.query as any).page) : 0;
-	const limit: number = (req.query as any).limit ? parseInt((req.query as any).limit) : 0;
 	try {
 		let { restaurantOrDishName } = req.body;
 
